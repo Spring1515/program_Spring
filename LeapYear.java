106f2ac4ae9d4d803b3eb2761fa1b76bee3a12a9
Spring1515 <106728117+Spring1515@users.noreply.github.com>
2022-06-02 16:15:47 +0900

    Add files via upload

---
+++ LeapYear.java
@@ -0,0 +1,24 @@
+/*****
+ * g2154622 Yuma Yoshida
+ * 2022 4/7
+ * 入力した年が閏年かどうかを判定する
+ *****/
+
+public class LeapYear {
+    public static void main(String[] args) {
+        Integer year = 2016;
+        Boolean leapyear = false;
+
+        if((year % 4 == 0) && (year % 100 != 0)) {  //4で割り切りて100でも割り切れるなら閏年
+            leapyear = true;
+        }else if(year % 400 == 0) {  //400で割り切れたら閏年
+            leapyear = true;
+        }
+
+        if(leapyear) {  //leapyearがtrueなら閏年
+            System.out.printf("%d年は閏年です。%n", year);
+        }else {  //leapyearがfalseなら閏年ではない
+            System.out.printf("%d年は閏年ではありません。%n", year);
+        }
+    }
+}
