2d7895aea114e28569ff61844a643c02ad3b82e2
Spring1515 <106728117+Spring1515@users.noreply.github.com>
2022-06-02 16:18:54 +0900

    インデントの整理

--- LeapYear.java
+++ LeapYear.java
@@ -1,7 +1,7 @@
 /*****
  * g2154622 Yuma Yoshida
  * 2022 4/7
- * 入力した年が閏年かどうかを判定する
+ * 入力した年が閏年かどうかを判断する
  *****/
 
 public class LeapYear {
@@ -9,15 +9,15 @@ public class LeapYear {
         Integer year = 2016;
         Boolean leapyear = false;
 
-        if((year % 4 == 0) && (year % 100 != 0)) {  //4で割り切りて100でも割り切れるなら閏年
+        if ((year % 4 == 0) && (year % 100 != 0)) { // 4で割り切りて100でも割り切れるなら閏年
             leapyear = true;
-        }else if(year % 400 == 0) {  //400で割り切れたら閏年
+        } else if (year % 400 == 0) { // 400で割り切れたら閏年
             leapyear = true;
         }
 
-        if(leapyear) {  //leapyearがtrueなら閏年
+        if (leapyear) { // leapyearがtrueなら閏年
             System.out.printf("%d年は閏年です。%n", year);
-        }else {  //leapyearがfalseなら閏年ではない
+        } else { // leapyearがfalseなら閏年ではない
             System.out.printf("%d年は閏年ではありません。%n", year);
         }
     }
