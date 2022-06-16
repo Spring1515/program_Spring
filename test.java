83d847e8225bc7ada3ad16faff1728f868142876
Spring1515 <jitianyoujiang2@gmail.com>
2022-06-16 16:37:00 +0900

    初期設定

---
+++ test.java
@@ -0,0 +1,10 @@
+public class test {
+    void run(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    public static void main(String[] args) {
+        test app = new test();
+        app.run(args);
+    }
+}
